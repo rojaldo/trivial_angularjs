7dcffda6832838888d442fb5b674d0ab961e4106
rojaldo <danielrojo@gmail.com>
2017-10-19 16:56:53 +0200

    data structure added

--- src/app/main/main.controller.js
+++ src/app/main/main.controller.js
@@ -1,4 +1,4 @@
-(function() {
+(function () {
   'use strict';
 
   angular
@@ -9,24 +9,49 @@
   function MainController($scope, $http, $log) {
     var vm = this;
 
-    vm.answer = function(answer, index){
-      if(answer === vm.details.results[index].correct_answer){
+    vm.cards = [];
+
+    vm.answer = function (answer, index) {
+      if (answer === vm.details.results[index].correct_answer) {
         $log.info('Right Answer');
-      }else{
+      } else {
         $log.info('Wrong Answer');
       }
     }
 
-    var onFetchComplete = function(success){
+    var onFetchComplete = function (success) {
       //angular return data in data attribute
       vm.details = success.data;
+      processData();
+
     };
-    
-    var onError = function(error){
+
+    var processData = function () {
+      for (var indexResults = 0; indexResults < vm.details.results.length; indexResults++) {
+        vm.cards.push(processCard(vm.details.results[indexResults]));
+      }
+    }
+
+    var processCard = function(myData) {
+      var myCard = {
+        question: myData.question,
+        answers: '',
+        rightAnswer:myData.correct_answer,
+        responded:false
+      };
+      myCard.answers = [];
+      myCard.answers = angular.copy(myData.incorrect_answers);
+      myCard.answers.push(myData.correct_answer);
+      return myCard;
+
+
+    }
+
+    var onError = function (error) {
       vm.error = "Unable to fetch the data: " + error;
     };
-    
+
     $http.get("https://opentdb.com/api.php?amount=10").
-    then(onFetchComplete,onError);
+    then(onFetchComplete, onError);
   }
 })();
