8e755a7a7852838c6251ec4bcce72414864f85d4
Daniel Rojo <danielrojo@gmail.com>
2017-10-24 10:09:02 +0200

    reponse feedback on buttons after answer

--- src/app/main/main.controller.js
+++ src/app/main/main.controller.js
@@ -1,57 +1,67 @@
-(function () {
-  'use strict';
+(function() {
+    'use strict';
 
-  angular
-    .module('trivialangular')
-    .controller('MainController', MainController);
+    angular
+        .module('trivialangular')
+        .controller('MainController', MainController);
 
-  /** @ngInject */
-  function MainController($scope, $http, $log) {
-    var vm = this;
+    /** @ngInject */
+    function MainController($scope, $http, $log) {
+        var vm = this;
 
-    vm.cards = [];
+        vm.cards = [];
 
-    vm.answer = function (answer, index) {
-      if (answer === vm.details.results[index].correct_answer) {
-        $log.info('Right Answer');
-      } else {
-        $log.info('Wrong Answer');
-      }
+        vm.answer = function(answer, cardIndex, answerIndex) {
+            vm.cards[cardIndex].responded = true;
+            if (answer === vm.cards[cardIndex].rightAnswer) {
+                vm.cards[cardIndex].buttonClass[answerIndex] = 'btn btn-large btn-block btn-success';
+                $log.info('Right Answer');
+
+            } else {
+                vm.cards[cardIndex].buttonClass[answerIndex] = 'btn btn-large btn-block btn-danger';
+                $log.info('Wrong Answer');
+            }
+        }
+
+        var onFetchComplete = function(success) {
+            //angular return data in data attribute
+            vm.details = success.data;
+            processData();
+
+        };
+
+        var processData = function() {
+            for (var indexResults = 0; indexResults < vm.details.results.length; indexResults++) {
+                vm.cards.push(processCard(vm.details.results[indexResults]));
+            }
+        }
+
+        var processCard = function(myData) {
+            var myCard = {
+                question: myData.question,
+                answers: [],
+                buttonClass: [],
+                rightAnswer: myData.correct_answer,
+                responded: false
+            };
+            myCard.answers = [];
+            myCard.answers = angular.copy(myData.incorrect_answers);
+            myCard.answers.push(myData.correct_answer);
+            myCard.buttonClass = ['btn btn-large btn-block btn-info',
+                'btn btn-large btn-block btn-info',
+                'btn btn-large btn-block btn-info',
+                'btn btn-large btn-block btn-info'
+            ];
+            return myCard;
+
+
+        }
+
+        var onError = function(error) {
+            vm.error = "Unable to fetch the data: " + error;
+        };
+
+        $http.get("https://opentdb.com/api.php?amount=10").
+        then(onFetchComplete, onError);
     }
-
-    var onFetchComplete = function (success) {
-      //angular return data in data attribute
-      vm.details = success.data;
-      processData();
-
-    };
-
-    var processData = function () {
-      for (var indexResults = 0; indexResults < vm.details.results.length; indexResults++) {
-        vm.cards.push(processCard(vm.details.results[indexResults]));
-      }
-    }
-
-    var processCard = function(myData) {
-      var myCard = {
-        question: myData.question,
-        answers: '',
-        rightAnswer:myData.correct_answer,
-        responded:false
-      };
-      myCard.answers = [];
-      myCard.answers = angular.copy(myData.incorrect_answers);
-      myCard.answers.push(myData.correct_answer);
-      return myCard;
-
-
-    }
-
-    var onError = function (error) {
-      vm.error = "Unable to fetch the data: " + error;
-    };
-
-    $http.get("https://opentdb.com/api.php?amount=10").
-    then(onFetchComplete, onError);
-  }
 })();
